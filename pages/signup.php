4efca2e082de7c57a49998c94b6191a4b0840ac9
Nell-JvR-251061 <251061@virtualwindow.co.za>
2026-08-20 07:38:19 +0200

    README Assets Push
    Push to add all visual elements to readme

--- pages/signup.php
+++ pages/signup.php
@@ -9,7 +9,7 @@
 
     <link rel="icon" type="image/x-icon" href="/polyplex/assets/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styling/login.css">
+    <link rel="stylesheet" href="../styling/signup.css">
 </head>
 
 <header>
@@ -19,23 +19,23 @@
 <body>
     <div class="login-title col-12 text-center fs-1 ubuntu-bold my-5 p-2">
         <span class="col-12 text-center">
-            LOGIN
+            SIGN-UP
         </span>
     </div>
 
     <div class="form-container d-flex justify-content-center">
         <form class="login-form col-6 p-4 d-flex flex-column">
             <div class="mb-4">
-                <label for="InputEmail" class="form-label ubuntu-bold fs-5">Email</label>
-                <input placeholder="example@email.com" type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
+                <label for="InputEmail1" class="form-label ubuntu-bold fs-5">Email</label>
+                <input placeholder="example@email.com" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
             <div class="mb-5">
-                <label for="InputPassword" class="form-label ubuntu-bold fs-5">Password</label>
-                <input placeholder="superSecret@here" type="password" class="form-control" id="exampleInputPassword">
+                <label for="InputPassword1" class="form-label ubuntu-bold fs-5">Password</label>
+                <input placeholder="superSecret@here" type="password" class="form-control" id="exampleInputPassword1">
             </div>
             <a type="submit" class="btn login-submit-btn fs-4" href="../pages/dashboard.php">Submit</a>
 
-            <span class="sign-up-link-text text-center mt-5">Don't have an account? <a href="./signup.php" class="sign-up-link ms-1"> Sign up here </a> </span>
+            <span class="sign-up-link-text text-center mt-5">Already have an account? <a href="./login.php" class="sign-up-link ms-1"> Log in here </a> </span>
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
